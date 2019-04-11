bf9ec361ed26d09cdddd753bd3f6a8b6febbeb02
H3rN4n <desouza030689@gmail.com>
2019-04-11 15:53:54 -0300

    aws s3 upload public

--- uploader.js
+++ uploader.js
@@ -1,74 +1,101 @@
 const AWS = require("aws-sdk");
-//*/ get reference to S3 client
-var s3 = new AWS.S3({ signatureVersion: "v4" });
-module.exports.uploader = (event, context, callback) => {
-  const base64Image = JSON.parse(event.body).avatar_image;
+const sharp = require("sharp");
+const s3 = new AWS.S3({ signatureVersion: "v4" });
+const btoa = require("btoa");
+const s3BucketName =
+  "s3-file-upload-dev-serverlessdeploymentbucket-btt7iylqaxbg"; //s3-file-upload-dev-serverlessdeploymentbucket-btt7iylqaxbg
+let bucketParams = { Bucket: s3BucketName, ACL: "public-read" };
 
-  console.log(base64Image);
+s3.createBucket(bucketParams)
+  .promise()
+  .then(console.log)
+  .catch(console.error);
+
+const thumbsSizes = [
+  { w: 400, h: 300 },
+  { w: 160, h: 120 },
+  { w: 120, h: 120 }
+];
+
+const resize = async (file, width, height) => {
+  let resized = await sharp(file)
+    .resize(width, height)
+    .toBuffer();
+  return resized;
+};
+
+const createThumbs = async (image, title, thumbsSizes) => {
+  let files = await Promise.all(
+    thumbsSizes.map(async size => {
+      const resizedImage = await resize(image, size.w, size.h);
+      var params = {
+        Body: resizedImage,
+        ContentEncoding: "base64",
+        ContentType: "image/jpg",
+        ACL: "public-read",
+        Bucket: s3BucketName,
+        Key: `thumbs/${title}-${size.w}x${size.h}.jpg`
+      };
+      let file = await s3.upload(params).promise();
+      return {
+        name: `${size.w}x${size.h}`,
+        url: file.Location
+      };
+    })
+  );
+  return files;
+};
+
+const errorHandler = message => {
+  return {
+    statusCode: 500,
+    body: JSON.stringify({
+      message: message
+    })
+  };
+};
+
+module.exports.uploader = async (event, context) => {
+  const req = JSON.parse(event.body);
+
+  if (!req.image) {
+    return errorHandler("image required");
+  }
+
+  if (!req.title.length) {
+    return errorHandler("title required");
+  }
+
+  const base64Image = req.image;
+  const title = req.title;
   let decodedImage = new Buffer(
     base64Image.replace(/^data:image\/\w+;base64,/, ""),
     "base64"
   );
 
-  //decodedImage.byteLength
-
-  var filePath = "avatars/" + event.queryStringParameters.username + ".jpg";
-
-  var params = {
-    Body: decodedImage,
-    ContentEncoding: "base64",
-    ContentType: "image/jpg",
-    Bucket: "s3-file-upload-dev-serverlessdeploymentbucket-btt7iylqaxbg",
-    Key: filePath
+  let response = async () => {
+    let files = await createThumbs(decodedImage, title, thumbsSizes);
+    return {
+      statusCode: 200,
+      headers: {
+        my_header: "my_value"
+      },
+      body: JSON.stringify({
+        title: req.title,
+        files: files
+      }),
+      isBase64Encoded: false
+    };
   };
 
-  console.log(decodedImage);
-
-  return s3.upload(params, (err, data) => {
-    if (err) {
-      console.log("err", err);
-      callback(err, null);
-      return {
-        statusCode: 500,
-        body: JSON.stringify({
-          message: err,
-          input: event
-        })
-      };
-    } else {
-      let response = {
-        statusCode: 200,
-        headers: {
-          my_header: "my_value"
-        },
-        body: JSON.stringify(data),
-        isBase64Encoded: false
-      };
-      console.log("response", response);
-      callback(null, response);
-      return response;
-    }
-  });
+  return await response();
 };
 
-module.exports.getFile = (event, context, callback) => {
-  var params = {
-    Bucket: "s3-file-upload-dev-serverlessdeploymentbucket-btt7iylqaxbg",
-    Key: event.queryStringParameters.key
-  };
-  s3.getObject(params, (err, data) => {
-    if (err) {
-      callback(err, null);
-    } else {
-      let response = {
-        statusCode: 200,
-        headers: {
-          my_header: "my_value"
-        },
-        body: JSON.stringify(data),
-        isBase64Encoded: false
-      };
-      callback(null, response);
-    }
-  });
+const bufferToBase64 = buf => {
+  var binstr = Array.prototype.map
+    .call(buf, function(ch) {
+      return String.fromCharCode(ch);
+    })
+    .join("");
+  return btoa(binstr);
 };
